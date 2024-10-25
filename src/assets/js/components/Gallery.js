765cf99e47c7e8ce88f08156938104b163b59893
Olivier Gorzalka <olivier@amphibee.fr>
2024-10-25 12:04:07 +0200

    Added disabled tag method

--- src/assets/js/components/Gallery.js
+++ src/assets/js/components/Gallery.js
@@ -1,66 +1,59 @@
 import portfolio from './../../../datas/portfolio.json';
-/**
- * Gallery component for displaying and filtering images.
- * @returns {Object} An object containing methods for initializing the gallery and adding/removing filters.
- */
+
 export default function Gallery() {
     return {
         images: [],
         tags: [],
         filterList: [],
+        availableTags: new Set(),
 
-        /**
-         * Initializes the gallery by setting the tags and images.
-         * @returns {void}
-         */
         init: function() {
-            this.tags = this.getUniqueCategories();
+            this.tags = this.getUniqueCategories(portfolio);
             this.images = portfolio.slice(0, 8);
+            this.availableTags = new Set(this.tags);
         },
 
-        /**
-         * Adds or removes a filter based on the provided tag.
-         * @param {string} tag - The tag to add or remove from the filter list.
-         * @returns {void}
-         */
         addOrRemoveFilter: function(tag) {
+            if (!this.isTagAvailable(tag)) return;
+
+            // Toggle the filterList
             if (this.isTagSelected(tag)) {
                 this.filterList = this.filterList.filter(filter => filter !== tag);
             } else {
                 this.filterList.push(tag);
             }
             this.filterImages();
-
+            this.updateAvailableTags();
         },
 
         isTagSelected: function(tag) {
             return this.filterList.includes(tag);
         },
 
-        /**
-         * Retrieves unique categories from the portfolio data.
-         * @returns {Array} An array of unique categories.
-         */
-        getUniqueCategories: function() {
-            const categories = new Set();
-            portfolio.forEach(image => {
-                if (!image.category) {return; }
+        isTagAvailable: function(tag) {
+            return this.availableTags.has(tag); // Use Set.has() for O(1) lookup
+        },
 
-                image.category.forEach(category => {
-                    categories.add(category);
-                });
+        getUniqueCategories: function(imageList) {
+            const categories = new Set();
+            imageList.forEach(image => {
+                if (image.category) {
+                    image.category.forEach(category => categories.add(category));
+                }
             });
             return Array.from(categories);
         },
 
         filterImages: function() {
             this.images = portfolio.filter(image => {
-                if (this.filterList.length === 0) {
-                    return true;
-                }
+                if (this.filterList.length === 0) return true;
                 const imageCategories = new Set(image.category);
                 return this.filterList.every(tag => imageCategories.has(tag));
             });
         },
-    }
+
+        updateAvailableTags: function() {
+            this.availableTags = new Set(this.getUniqueCategories(this.images));
+        },
+    };
 }
