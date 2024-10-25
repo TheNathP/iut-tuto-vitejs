03782b4b961d5d070a49fc2b5a3b74083518fdf7
Nathan <nathanpiet2004@gmail.com>
2024-10-25 16:40:17 +0200

    bientot la fin

--- src/assets/js/bootstrap.js
+++ src/assets/js/bootstrap.js
@@ -1,8 +1,11 @@
 import Alpine from 'alpinejs';
+import intersect from '@alpinejs/intersect';
 import Slider from './components/Slider.js';
 import Gallery from './components/Gallery.js';
 
+
 window.Alpine = Alpine;
+Alpine.plugin(intersect);
 
 Alpine.data('Slider', Slider);
 Alpine.data('Gallery', Gallery);
