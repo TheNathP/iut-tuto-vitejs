c2f2595e6df9f2040273041b499f3eebff0453f1
Olivier Gorzalka <olivier@amphibee.fr>
2024-10-25 16:39:47 +0200

    Added tab + appearance animation

--- src/assets/js/bootstrap.js
+++ src/assets/js/bootstrap.js
@@ -1,9 +1,11 @@
 import Alpine from 'alpinejs';
 import Slider from './components/Slider.js';
 import Gallery from './components/Gallery.js';
+import {intersect} from "@alpinejs/intersect";
 
 window.Alpine = Alpine;
 
 Alpine.data('Slider', Slider);
 Alpine.data('Gallery', Gallery);
+Alpine.plugin(intersect)
 Alpine.start();
